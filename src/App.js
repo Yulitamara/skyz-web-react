86b59a86e70184f82d0e048f2c1ff6182085f157
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2023-06-25 12:58:30 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Blogs from "./pages/blogs/Blogs";
+import Blogs from "./pages/Blogs";
 import Navbar from "./components/Navbar";
-import Blog from "./pages/blog/Blog";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blogs" element={<Blogs />} />
-          <Route path="/blog/:id" element={<Blog />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,16 +1,22 @@
 import { Helmet } from "react-helmet-async";
-import "./blogs.css";
+import "../assets/scss/_blogs.scss";
+import {PostMasonry} from "../components/common";
+import trending from "../assets/mocks/trending.js"
 
 const Blogs = () => {
   return (
-    <div className="blogs">
+    <section className="blogs">
       <Helmet>
         <title>SkyzCRM</title>
         <meta name="description" content="אתר הבלוגים של Skyz CRM"></meta>
         <link rel="canonical" href="/בלוג"></link>
       </Helmet>
       <h1>Skyz Blog</h1>
-    </div>
+      <div className="row">
+        <h2>Trending Posts</h2>
+        <PostMasonry posts={trending} columns={3} />
+      </div>
+    </section>
   );
 };
 
