877999f3e90a58cdd730b300f5834056e1207ff8
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2023-11-08 09:58:55 +0200

    advantages translate

--- src/components/Advantages.js
+++ src/components/Advantages.js
@@ -8,98 +8,64 @@ const Advantages = () => {
         <div className="icon-container">
           <span className="material-symbols-outlined">cloud</span>
         </div>
-        <h2 className="header">Cloud technology</h2>
-        <p>
-          Move to an innovative cloud-based system. Enable employees to work
-          from anywhere, on any device, in real time.
-        </p>
+        <h2 className="header">{t("advantages-cloud-t")}</h2>
+        <p>{t("advantages-cloud-p")}</p>
       </div>
       <div className="advantage">
         <div className="icon-container">
           <span className="material-symbols-outlined">sync</span>
         </div>
-        <h2 className="header">360-degree customer view</h2>
-        <p>
-          Get a comprehensive view of your customers in one glance: current
-          status, history, processes, inquiries, opportunities, calls, and
-          deals.
-        </p>
+        <h2 className="header">{t("advantages-360-t")}</h2>
+        <p>{t("advantages-360-p")}</p>
       </div>
       <div className="advantage">
         <div className="icon-container">
           <span className="material-symbols-outlined">devices</span>
         </div>
-        <h2 className="header">Work from anywhere</h2>
-        <p>
-          View critical information, drive action, and work with customers
-          effectively from the office, the field, the road, home, or abroad.
-        </p>
+        <h2 className="header">{t("advantages-anywhere-t")}</h2>
+        <p>{t("advantages-anywhere-p")}</p>
       </div>
       <div className="advantage">
         <div className="icon-container">
           <span className="material-symbols-outlined">watch</span>
         </div>
-        <h2 className="header">Real-time management</h2>
-        <p>
-          Manage processes, tasks, supervision, and control online from anywhere
-          with immediate access to information. Every minute is worth a lot of
-          money.
-        </p>
+        <h2 className="header">{t("advantages-real-time-t")}</h2>
+        <p>{t("advantages-real-time-p")}</p>
       </div>
       <div className="advantage">
         <div className="icon-container">
           <span className="material-symbols-outlined">view_array</span>
         </div>
-        <h2 className="header">User-friendly display</h2>
-        <p>
-          Give yourself a convenient and optimal view of information, data,
-          reports, and dashboards on any device, anywhere, at the touch of a
-          button.
-        </p>
+        <h2 className="header">{t("advantages-friendly-t")}</h2>
+        <p>{t("advantages-friendly-p")}</p>
       </div>
       <div className="advantage">
         <div className="icon-container">
           <span className="material-symbols-outlined">wifi_tethering</span>
         </div>
-        <h2 className="header">Instant system access</h2>
-        <p>
-          Connect to Skyz CRM using the SaaS model and start working tomorrow
-          morning. Save yourself the expense of expensive purchases and the
-          hassle of data security.
-        </p>
+        <h2 className="header">{t("advantages-instant-t")}</h2>
+        <p>{t("advantages-instant-p")}</p>
       </div>
       <div className="advantage">
         <div className="icon-container">
           <span className="material-symbols-outlined">highlight</span>
         </div>
-        <h2 className="header">Innovative approach</h2>
-        <p>
-          Upgrade your organization through an innovative management system. Get
-          answers to every need in the modern era in terms of work, technology,
-          marketing, service, and collaboration.
-        </p>
+        <h2 className="header">{t("advantages-innovative-t")}</h2>
+        <p>{t("advantages-innovative-p")}</p>
       </div>
       <div className="advantage">
         <div className="icon-container">
           <span className="material-symbols-outlined">phone_iphone</span>
         </div>
-        <h2 className="header">Full mobile compatibility</h2>
-        <p>
-          Full mobile compatibility Allow yourself to manage customer
-          relationships from mobile devices just like from the office. Take
-          advantage of field work capabilities and increase productivity.
-        </p>
+        <h2 className="header">{t("advantages-mobile-t")}</h2>
+        <p>{t("advantages-mobile-p")}</p>
       </div>
       <div className="advantage">
         <div className="icon-container">
           <span className="material-symbols-outlined">equalizer</span>
         </div>
-        <h2 className="header">High ROI</h2>
-        <p>
-          Dramatically improve work processes. Increase revenue and
-          significantly improve the bottom line on a monthly, quarterly, and
-          annual basis.
-        </p>
+        <h2 className="header">{t("advantages-roi-t")}</h2>
+        <p>{t("advantages-roi-p")}</p>
       </div>
     </section>
   );
