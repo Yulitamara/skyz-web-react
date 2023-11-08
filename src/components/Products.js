6e2c3cf808acca57685a6d2ee0971166fc0e658d
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2023-11-08 15:57:34 +0200

    imgs

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,26 @@
-import sales from "../assets/imgs/products-imgs/sales.png";
-import marketing from "../assets/imgs/products-imgs/marketing.png";
-import service from "../assets/imgs/products-imgs/service.png";
+import salesLight from "../assets/imgs/products-imgs/sales-light.png";
+import salesDark from "../assets/imgs/products-imgs/sales-dark.png";
+import marketingLight from "../assets/imgs/products-imgs/marketing-light.png";
+import marketingDark from "../assets/imgs/products-imgs/marketing-dark.png";
+import serviceLight from "../assets/imgs/products-imgs/service-light.png";
+import serviceDark from "../assets/imgs/products-imgs/service-dark.png";
+
 import "../assets/scss/_products.scss";
 import "../assets/scss/base.scss";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-const Products = () => {
+const Products = ({ darkMode }) => {
+  const salesImg = darkMode ? salesDark : salesLight;
+  const marketingImg = darkMode ? marketingDark : marketingLight;
+  const serviceImg = darkMode ? serviceDark : serviceLight;
+
   const { t } = useTranslation();
   return (
     <section className="products">
       <div className="product">
         <div className="header">
-          <img src={sales} alt="sales img" />
+          <img src={salesImg} alt="sales img" />
           <h2>{t("sales-header")}</h2>
         </div>
         <p>{t("sales-content")}</p>
@@ -22,7 +30,7 @@ const Products = () => {
       </div>
       <div className="product">
         <div className="header">
-          <img src={marketing} alt="marketing img" />
+          <img src={marketingImg} alt="marketing img" />
           <h2>{t("marketing-header")}</h2>
         </div>
         <p>{t("marketing-content")}</p>
@@ -32,7 +40,7 @@ const Products = () => {
       </div>
       <div className="product">
         <div className="header">
-          <img src={service} alt="service img" />
+          <img src={serviceImg} alt="service img" />
           <h2>{t("service-header")}</h2>
         </div>
         <p>{t("service-content")}</p>
